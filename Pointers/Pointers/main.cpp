cf4ae030b284d0e39cf964d43e12a4339a1b086b
Luciano <passos.luciano@outlook.com>
2018-05-27 17:44:42 -0300

    Adding pointers section lessons

---
+++ Pointers/Pointers/main.cpp
@@ -0,0 +1,81 @@
+//
+//  main.cpp
+//  Pointers
+//
+//  Created by Luciano Almeida on 27/05/18.
+//  Copyright © 2018 Luciano Almeida. All rights reserved.
+//
+
+#include <iostream>
+// Reference variable
+void mutateRef(int &ref) {
+    ref = 123;
+}
+
+void aritimetics1() {
+    int raw = 435635;
+    
+    int hours, minutes, seconds;
+    hours = raw/3600;
+    minutes = (raw%3600)/60;
+    seconds = (raw%3600)%60;
+    
+    std::cout << hours << " : " << minutes << " : " << seconds << std::endl;
+}
+
+void aritmetics2() {
+    for (int i = 0; i <= 10000; i++) {
+        if (i%100 == 0) {
+            std::cout << "i = " << i << " ..." << std::endl;
+        }
+    }
+}
+
+void pointerIteration() {
+    std::string texts[] = {"One", "Two", "Three"};
+    std::string* pTexts = texts;
+    
+    for (int i = 0; i < 3; i++) {
+        std::cout << i << " = " << *(pTexts + i) << std::endl;
+    }
+}
+
+void charArray() {
+    char text[] = "hello world";
+    
+    for (int i = 0; i < sizeof(text); i++) {
+        std::cout << text[i] << " " << std::flush;
+    }
+    std::cout << std::endl;
+    int aux = 0;
+    do {
+        std::cout << text[aux] << " " << std::flush;
+        aux++;
+    } while(text[aux]);
+    std::cout << std::endl;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int value = 6;
+    mutateRef(value);
+    std::cout << value << std::endl;
+    
+    double dValue = (double)7/2;
+    std::cout << "Double Division: " << dValue << std::endl;
+    
+    int iValue = 9;
+    std::cout << "Int Double: " << iValue << std::endl;
+    
+    aritimetics1();
+    aritmetics2();
+    
+    pointerIteration();
+    
+    charArray();
+    return 0;
+}
+
+
+
+
