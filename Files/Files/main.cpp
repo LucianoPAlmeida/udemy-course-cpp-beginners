588333f230d14cc989b6cbce7ed6540f6c607778
Luciano Almeida <passos.luciano@outlook.com>
2018-10-10 00:08:15 -0300

    Changes.

--- Files/Files/main.cpp
+++ Files/Files/main.cpp
@@ -11,30 +11,27 @@
 
 int main(int argc, const char * argv[]) {
     
-//    std::fstream f;
-//    f.open("./text.txt", std::fstream::app);
-//    if (f.is_open()) {
-//        std::string in;
-//        std::cin >> in;
-//        f << in << std::endl;
-//
-//        f.close();
-//    }
+    std::fstream f;
+    f.open("./text.txt", std::fstream::app);
+    if (f.is_open()) {
+        f << "Hue" << std::endl;
+        f.close();
+    }
     
     
     std::fstream file;
     file.open("./text1.txt");
     if (file.is_open()) {
         std::string buff;
-        
+
         do {
             file >> buff;
             std::cout << buff << std::endl;
         } while(!file.eof());
-        
+
         std::cout << "rdstate: " << file.rdstate() << std::endl;
         std::cout << "eofbit & failbit: " << (std::ifstream::eofbit | std::ifstream::failbit) << std::endl;
-        
+
         if (file.rdstate() == (std::ifstream::eofbit | std::ifstream::failbit)) {
             std::cout << "Error: eof() and fail()" << std::endl;
         }
